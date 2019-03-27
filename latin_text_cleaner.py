3a5c419007011c210cd169d35d62d12628f68e15
holmesr19 <holmesr19@mail.wlu.edu>
2019-03-26 21:18:04 -0400

    all text condensed to one file, lemmatization

--- latin_text_cleaner.py
+++ latin_text_cleaner.py
@@ -5,15 +5,37 @@ writes the output to a file
 bancks holmes
 '''
 import string
-import numpy
+import numpy as np
+from cltk.lemmatize.latin.backoff import BackoffLatinLemmatizer
+import os
 
-file = open('ovid/sample_text.txt', 'rb')
+#first, build list of filenames
+filenames = []
+for filename in os.listdir('ovid'):
+    filenames.append(str(filename))
+print(filenames)
+#then, concatenate them into one text file https://stackoverflow.com/questions/13613336/python-concatenate-text-files
+with open('corpora/all.txt', 'w') as outfile:
+    for fname in filenames:
+        with open('ovid/' + fname, 'r') as infile:
+            for line in infile:
+                outfile.write(line)
+print('outfile written')
+
+lemmatizer = BackoffLatinLemmatizer()
 ltr_str = ''
+
+file = open('ovid/sample_text.txt', 'r')
 for line in file:
     ltr_str += str(line)
-
-out = ltr_str.translate(str.maketrans("",""), string.punctuation) #import numpy to use for this
-
 file.close()
 
-print(out)
+np_str = np.asarray(ltr_str)   
+
+for symbol in string.punctuation:
+    np_str = np.char.replace(np_str, symbol, '')
+
+np_str = np.char.lower(np_str)
+tokens = np_str.tolist().split()
+lemmatized = lemmatizer.lemmatize(tokens)
+#print(filenames)
