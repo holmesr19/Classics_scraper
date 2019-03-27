0c5424c30ede7dd0b83b86594c600377b9e32ae1
holmesr19 <holmesr19@mail.wlu.edu>
2019-03-27 13:53:09 -0400

    visualizer plots characters instead of words

--- latin_text_visualizer.py
+++ latin_text_visualizer.py
@@ -9,11 +9,36 @@ from sklearn.decomposition import PCA
 from matplotlib import pyplot
 
 #read lemmata into list of strings
+def read_file(path):
+    return_str = ''
+    with open(path, 'r') as file:
+        for line in file:
+            return_str += line
+    return return_str.split()
 
 #make model using w2v
+def w2v(lemmata):
+    return Word2Vec(lemmata)
 
 #flatten with PCA
+def make_pca(model):
+    X = model[model.wv.vocab]
+    pca = PCA(n_components = 2)
+    result = pca.fit_transform(X)
+    return result
 
 #make graph
-
+def plot(data, model, pca_result):
+    pyplot.scatter(pca_result[:, 0], pca_result[:, 1])
+    words = list(model.wv.vocab)
+    for i, word in enumerate(words):
+        pyplot.annotate(word, xy = (pca_result[i, 0], pca_result[i, 1]))
+    pyplot.show()
 #where to limit to the 100 (or more?) most used words?
+
+#stitch these all together
+her_data = read_file('corpora/her_lemmatized.txt')
+print(her_data)
+her_model = w2v(her_data)
+her_pca = make_pca(her_model)
+plot(her_data, her_model, her_pca)
