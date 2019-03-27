6ac8fa1887a95f42b06541f804d28558e37bd160
holmesr19 <holmesr19@mail.wlu.edu>
2019-03-26 23:03:04 -0400

    set up imports and outlined logic

---
+++ latin_text_visualizer.py
@@ -0,0 +1,19 @@
+'''
+latin_text_visualizer.py
+reads lemmata in from a file and creates word embeddings, then flattens
+them into 2d with PCA to graph them
+bancks holmes
+'''
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+
+#read lemmata into list of strings
+
+#make model using w2v
+
+#flatten with PCA
+
+#make graph
+
+#where to limit to the 100 (or more?) most used words?
