339084986d6f80fb1ade0814ac80eb838c6b49e3
holmesr19 <holmesr19@mail.wlu.edu>
2019-03-28 22:40:34 -0400

    tweak parameters for word2vec model

--- latin_text_visualizer.py
+++ latin_text_visualizer.py
@@ -8,6 +8,16 @@ from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
 from nltk import word_tokenize
+import seaborn as sns
+sns.set()
+
+def stopwords():
+    stopwords = ''
+    with open('stopwords.txt', 'r') as file:
+        for line in file:
+            stopwords += line
+    #print(stopwords.split())
+    return stopwords.split()
 
 #read lemmata into list of strings
 def read_file(path):
@@ -22,9 +32,9 @@ def w2v(lemmata):
     token_list = []
     tokens = word_tokenize(lemmata)
     #remove outliers
-    tokens = [ elem for elem in tokens if elem not in ['p', 'ovidi', 'nasonis', 'epitvlae', 'heroidvm', 'herois', 'pagus', 'classics', 'library', 'fero', 'thos', 'ovid']] 
+    tokens = [ elem for elem in tokens if elem not in ['p', 'ovidi', 'nasonis', 'epitvlae', 'heroidvm', 'herois', 'pagus', 'classics', 'library', 'fero', 'thos', 'ovid'] and elem not in stopwords()] 
     token_list.append(tokens)
-    return Word2Vec(token_list, min_count = 50)    #an optional min_count argument is responsible for population control
+    return Word2Vec(token_list, min_count = len(tokens)/500, window = 15)    #an optional min_count argument is responsible for population control
 
 #flatten with PCA
 def make_pca(model):
@@ -39,8 +49,11 @@ def plot(data, model, pca_result):
     words = list(model.wv.vocab)
     for i, word in enumerate(words):
         pyplot.annotate(word, xy = (pca_result[i, 0], pca_result[i, 1]))
-    pyplot.show()
+    
 
+
+    
+        
 
 """ #stitch these all together into 1 function once it works
 her_data = read_file('corpora/her_lemmatized.txt')
@@ -58,4 +71,10 @@ def stitch(path):
     plot(word_data, word_model, word_pca)
 
 stitch('corpora/all_lemmatized.txt')
+pyplot.show()
 stitch('corpora/her_lemmatized.txt')
+pyplot.title('heroides v. all, min_count: len/500, window: 10')
+#pyplot.gca().legend(('all', 'heroides'))
+pyplot.show()
+
+#cross shape is accurate, make sure to use set axes for final graphs
